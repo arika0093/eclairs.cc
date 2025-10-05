2184918c424b39f40ec4d133e14f7335f7e82b7c
Arika Ishinami <delete0093@gmail.com>
2025-10-06 00:40:52 +0900

    feat: add Header component with navigation links for GitHub and Zenn

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
-<ThemeToggle />
-<Hero />
-
 <script>
+  import Header from "./lib/Header.svelte";
   import Hero from "./lib/Hero.svelte";
   import ThemeToggle from "./lib/ThemeToggle.svelte";
 </script>
+
+<Header />
+<ThemeToggle />
+<Hero />
 
